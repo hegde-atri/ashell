0739590088b67c3d487a564cb97b8326408b1605
Atri Hegde <iamatrihegde@outlook.com>
2026-07-06 09:54:32 +0100

    feat: read user input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io::{self, stdin, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+  // REPL Loop
+  loop {
+    // Print prompt
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // Handle input from user
+    let mut input = String::new();
+    match stdin().read_line(&mut input) {
+      Ok(_) => {},
+      Err(err) => {
+        println!("Failed to read input: {err}");
+      }
+    }
+
+    // Evaluate input
+    println!("{}: command not found", input.trim());
+  }
 }
